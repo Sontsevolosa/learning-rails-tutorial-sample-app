4e2bccb54be880d9802f18750d312e4ac19f9552
sontsevolosa <sontsevolosa@gmail.com>
2016-05-16 15:25:50 +0300

    Adding a digest method for use in fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ class User < ActiveRecord::Base
     def User.digest(string)
     	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
     												  BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
 	end
 end

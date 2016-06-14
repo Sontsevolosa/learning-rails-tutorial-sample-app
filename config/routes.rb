ddd76a3946d90121972497b0e2b946eaff3a7fe3
sontsevolosa <sontsevolosa@gmail.com>
2016-06-14 19:41:16 +0300

    Adding the routes for user relationships

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
 
   resources :microposts, only: [:create, :destroy]
   
+  resources :relationships, only:[:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
